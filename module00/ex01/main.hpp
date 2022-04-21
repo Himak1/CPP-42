3bfb1f0b6904deca2ad957607063a0ee6fac257f
Justin Hille <jhille@f1r1s9.codam.nl>
2022-04-21 12:43:11 +0200

    added phonebook class, nede to implement the functions

--- module00/ex01/main.hpp
+++ module00/ex01/main.hpp
@@ -1,38 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   phonebook.hpp                                      :+:    :+:            */
+/*   main.hpp                                           :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: jhille <jhille@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2022/04/20 18:12:55 by jhille        #+#    #+#                 */
-/*   Updated: 2022/04/21 12:39:50 by jhille        ########   odam.nl         */
+/*   Created: 2022/04/21 12:34:49 by jhille        #+#    #+#                 */
+/*   Updated: 2022/04/21 12:42:40 by jhille        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHONEBOOK_HPP
-# define PHONEBOOK_HPP
-# include <string>
+#include <iostream>
+#include "phonebook.hpp"
 
-class	Contact
+int main(void)
 {
-	public:
-		int			index;
-		std::string	first_name;
-		std::string	last_name;
-		std::string	nickname;
-		std::string	phone_number;
-		std::string	darkest_secret;
-		
-};
+	PhoneBook	phonebook;
+	std::string	cmd;
 
-class	PhoneBook
-{
-	public:
-		Contact	contacts[8];
-		void	add_ct(void);
-		void	search_ct(void);
-		void	delete_ct(void);
-};
-
-#endif
+	while (1)
+	{
+		std::cin >> cmd;
+		if (!cmd.compare("ADD"))
+            phonebook.add_ct();
+        else if (!cmd.compare("SEARCH"))
+            phonebook.search_ct();
+        else if (!cmd.compare("DELETE"))
+            phonebook.delete_ct();
+        
+    }
+}
