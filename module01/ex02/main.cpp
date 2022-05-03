3f88d1d6ad4d7453e1d07c348b4fdbadb2faac36
Justin Hille <jhille@f0r1s10.codam.nl>
2022-05-03 10:54:11 +0200

    finished ex02

--- module01/ex02/main.cpp
+++ module01/ex02/main.cpp
@@ -6,17 +6,25 @@
 /*   By: jhille <jhille@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/05/02 16:35:41 by jhille        #+#    #+#                 */
-/*   Updated: 2022/05/02 16:42:08 by jhille        ########   odam.nl         */
+/*   Updated: 2022/05/03 10:53:34 by jhille        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Zombie.hpp"
+#include <string>
+#include <iostream>
 
 int main(void)
 {
 	std::string		brain = "HI THIS IS BRAIN";
 	std::string*	stringPTR = &brain;
 	std::string&	stringREF = brain;
-	
+
+	std::cout << (void*)&brain << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << (void*)&stringREF << std::endl << std::endl;
+
+	std::cout << brain << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
 	return (0);
 }
