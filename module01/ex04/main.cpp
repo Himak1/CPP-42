982221dcb012dffc510638d09fbd648c9883750a
Justin Hille <jhille@f1r2s9.codam.nl>
2022-05-17 12:11:49 +0200

    ex04 is sorta working. Needs toe be tested

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -6,29 +6,51 @@
 /*   By: jhille <jhille@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/05/12 12:56:45 by jhille        #+#    #+#                 */
-/*   Updated: 2022/05/12 17:24:42 by jhille        ########   odam.nl         */
+/*   Updated: 2022/05/17 12:10:41 by jhille        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include "file_io.h"
+#include <fstream>
 
+static int	createOutputFile(std::fstream& outputFile, const char* inputFilename)
+{
+	std::string		outputFilename(inputFilename);
 
+	outputFilename.append(".replace");
+	outputFile.open(outputFilename, std::ios::out);
+	if (outputFile.fail())
+		return (-1);
+	return (0);
+}
 
 int main(int argc, char *argv[])
 {
 	std::fstream	inputFile;
 	std::fstream	outputFile;
-	std::string		*linesbuffer;
+	std::string		needle;
+	std::string		line;
+	int				needle_index;
 
 	if (argc < 4)
 		return (-1);
-	linesbuffer = createBuffer(argv[1]);
+	needle = std::string(argv[2]);
 	inputFile.open(argv[1]);
 	if (createOutputFile(outputFile, argv[1]) != -1)
 	{
-		
+		while (getline(inputFile, line))
+		{
+			needle_index = line.find(needle);
+			if (needle_index != -1)
+			{
+				line.insert(needle_index, std::string(argv[3]));
+				needle_index = line.find(needle);
+				line.erase(needle_index, needle.size());
+			}
+			outputFile << line << std::endl;
+		}
+		outputFile.close();
 	}
-	delete linesbuffer;
+	inputFile.close();
 	return (0);
 }
