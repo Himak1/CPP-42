e8c9a08dbba4efe6a559800668f357c2d4bda2b9
Justin Hille <jhille@f1r2s9.codam.nl>
2022-05-30 15:34:20 +0200

    it's done

--- module02/ex02/main.cpp
+++ module02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jhille <jhille@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/05/25 16:03:01 by jhille        #+#    #+#                 */
-/*   Updated: 2022/05/30 15:29:43 by jhille        ########   odam.nl         */
+/*   Updated: 2022/05/30 15:33:38 by jhille        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 
 int	main( void ) {
-	Fixed	a( 42.42f );
-	Fixed	b( 42.25f );
+	Fixed	x( 42.42f );
+	Fixed	y( 42.25f );
 
-	if (a > b)
-		std::cout << a << " is bigger than " << b << std::endl;
-	if (b < a)
-		std::cout << b << " is smaller than " << a << std::endl;
+	if (x > y)
+		std::cout << x << " is bigger than " << y << std::endl;
+	if (y < x)
+		std::cout << y << " is smaller than " << x << std::endl;
 
 	Fixed	t2( 10.50f );
 	Fixed	t3( 5.25f );
@@ -36,7 +36,20 @@ int	main( void ) {
 	std::cout << t4 << " + " << t5 << " = " << (t4 + t5) << std::endl;
 	std::cout << t4 << " - " << t5 << " = " << (t4 - t5) << std::endl;
 	std::cout << t4 << " * " << t5 << " = " << (t4 * t5) << std::endl;
-	std::cout << t4 << " / " << t5 << " = " << (t4 / t5) << std::endl;
+	std::cout << t4 << " / " << t5 << " = " << (t4 / t5) << std::endl << std::endl;
+
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	
+	std::cout << b << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
+	return 0;
 	return (0);
 }
 
