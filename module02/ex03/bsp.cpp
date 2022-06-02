bfd9e95ab6e833e489335ee8479125a71043041c
Justin Hille <jhille@f1r2s9.codam.nl>
2022-06-02 14:24:28 +0200

    mad alternative which still doesn't work

--- module02/ex03/bsp.cpp
+++ module02/ex03/bsp.cpp
@@ -6,13 +6,71 @@
 /*   By: jhille <jhille@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/01 13:11:00 by jhille        #+#    #+#                 */
-/*   Updated: 2022/06/01 17:38:06 by jhille        ########   odam.nl         */
+/*   Updated: 2022/06/02 14:22:43 by jhille        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 #include <cmath>
 
+bool	bsp( Point const a, Point const b, Point const c, Point const point ) {
+	std::cout << "a:     " <<  a << std::endl;
+	std::cout << "b:     " << b << std::endl;
+	std::cout << "c:     " << c << std::endl;
+	std::cout << "point: " << point << std::endl;
+
+	Fixed	w1;
+	Fixed	w2;
+	float	ca_y = c.getY() - a.getY();
+	float	pa_y = point.getY() - a.getY();
+	float	ba_y = b.getY() - a.getY();
+	float	ba_x = b.getX() - a.getX();
+	float	ca_x = c.getX() - a.getX();
+
+	w1 = ( (a.getX() * ca_y) + (pa_y * ca_x) - (point.getX() * ca_y) ) /
+		( (ba_y * ca_x) - (ba_x * ca_y) );
+
+	if (ca_y != 0)
+		w2 = (pa_y - (w1.toFloat() * ba_y)) / ca_y;
+	else
+		w2 = (pa_y - w1.toFloat() * ba_y) / 1;
+	std::cout << "w1: " << w1 << std::endl;
+	std::cout << "w2: " << w2 << std::endl;
+	if (w1 >= 0 && w2 >= 0)
+		return (true);
+	return (false);
+}
+
+/*
+bool	bsp( Point const a, Point const b, Point const c, Point const point ) {
+	std::cout << "a:     " <<  a << std::endl;
+	std::cout << "b:     " << b << std::endl;
+	std::cout << "c:     " << c << std::endl;
+	std::cout << "point: " << point << std::endl;
+
+	Fixed	w1;
+	Fixed	w2;
+	float	ca_y = c.getY() - a.getY();
+	float	pa_y = point.getY() - a.getY();
+	float	ba_y = b.getY() - a.getY();
+	float	ba_x = b.getX() - a.getX();
+	float	ca_x = c.getX() - a.getX();
+
+	w1 = ( (a.getX() * ca_y) + (pa_y * ca_x) - (point.getX() * ca_y) ) /
+		( (ba_y * ca_x) - (ba_x * ca_y) );
+
+	if (ca_y != 0)
+		w2 = (pa_y - (w1.toFloat() * ba_y)) / ca_y;
+	else
+		w2 = (pa_y - w1.toFloat() * ba_y) / 1;
+	std::cout << "w1: " << w1 << std::endl;
+	std::cout << "w2: " << w2 << std::endl;
+	if (w1 >= 0 && w2 >= 0)
+		return (true);
+	return (false);
+}
+*/
+/*
 bool	bsp( Point const a, Point const b, Point const c, Point const point ) {
 	std::cout << "a:     " <<  a << std::endl;
 	std::cout << "b:     " << b << std::endl;
@@ -30,9 +88,12 @@ bool	bsp( Point const a, Point const b, Point const c, Point const point ) {
 
 	w2 = (point.getY() - a.getY() - (w1.toFloat() * (b.getY() - a.getY()))) /
 		(c.getY() - a.getY());
+	
+	
 	std::cout << "w1: " << w1 << std::endl;
 	std::cout << "w2: " << w2 << std::endl;
 	if (w1 >= 0 && w2 >= 0)
 		return (true);
 	return (false);
 }
+*/
--- module02/ex03/main.cpp
+++ module02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jhille <jhille@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/05/30 15:44:15 by jhille        #+#    #+#                 */
-/*   Updated: 2022/06/01 17:49:30 by jhille        ########   odam.nl         */
+/*   Updated: 2022/06/02 14:08:45 by jhille        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	main(void) {
 	Point	a( -4.0f, 0.0f );
 	Point	b( 0.0f, 6.0f );
 	Point	c( 4.0f, 0.0f );
-	Point	point( 2.1f, 2.0f );
+	Point	point( 4.0f, 0.0f );
 
 	std::cout << bsp(a, b, c, point) << std::endl;
 	return (0);	
