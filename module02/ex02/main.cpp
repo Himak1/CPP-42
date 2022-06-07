de908e4b15683c4ed9098d7398e9bd07b0a9316b
Justin Hille <jhille@f1r1s9.codam.nl>
2022-06-07 14:41:12 +0200

    done

--- module02/ex02/main.cpp
+++ module02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jhille <jhille@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/05/25 16:03:01 by jhille        #+#    #+#                 */
-/*   Updated: 2022/05/30 15:33:38 by jhille        ########   odam.nl         */
+/*   Updated: 2022/06/07 14:38:00 by jhille        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,21 +52,3 @@ int	main( void ) {
 	return 0;
 	return (0);
 }
-
-
-/*
-int	main( void ) {
-	Fixed a;
-	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-	
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-	
-	std::cout << b << std::endl;
-	std::cout << Fixed::max( a, b ) << std::endl;
-	return 0;
-}
-*/
