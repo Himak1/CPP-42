1501c077ed5278b210a2be9990e2f52c0c93676c
Justin Hille <jhille@f1r1s9.codam.nl>
2022-06-22 13:53:17 +0200

    implemented the last changes. lets see what the eval sheet says

--- module04/ex01/Cat.cpp
+++ module04/ex01/Cat.cpp
@@ -6,7 +6,7 @@
 /*   By: jhille <jhille@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/13 13:42:21 by jhille        #+#    #+#                 */
-/*   Updated: 2022/06/15 16:34:41 by jhille        ########   odam.nl         */
+/*   Updated: 2022/06/22 12:51:21 by jhille        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ Cat::Cat()
 	type = "Cat";
 }
 
-Cat::Cat( const Cat & src )
+Cat::Cat( const Cat& src )
 {
 	std::cout << "A cat has been copied" << std::endl;
 	*this = src;
@@ -27,15 +27,6 @@ Cat::Cat( const Cat & src )
 Cat::~Cat()
 {
 	std::cout << "A cat has been deconstructed" << std::endl;
-}
-
-Cat &	Cat::operator=( Cat const & rhs )
-{
-	if ( this != &rhs )
-	{
-		this->type = rhs.type;
-	}
-	return *this;
 }
 
 void	Cat::makeSound() const
