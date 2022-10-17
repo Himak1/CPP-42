dddf4dbf783d8d247910e42b4a4610f632d70c01
Justin Hille <jhille@f1r1s10.codam.nl>
2022-10-17 16:39:33 +0200

    Added the Base and A, B, C classes to module 06 ex02

--- module06/ex02/Base.cpp
+++ module06/ex02/Base.cpp
@@ -6,19 +6,76 @@
 /*   By: jhille <jhille@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/14 17:35:19 by jhille        #+#    #+#                 */
-/*   Updated: 2022/10/17 11:44:37 by jhille        ########   odam.nl         */
+/*   Updated: 2022/10/17 16:38:30 by jhille        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <time>
 #include <cstdlib>
+#include <ctime>
 #include "Base.hpp"
+
+/* #region ------------------ Base ------------------ */
+
+Base::~Base()
+{
+}
+
+/* #endregion */
+
+/* #region ------------------ A ------------------ */
+
+A::A()
+{
+}
+
+A::~A()
+{
+}
+
+/* #endregion */
+
+/* #region ------------------ B ------------------ */
+
+B::B()
+{
+}
+
+B::~B()
+{
+}
+
+/* #endregion */
+
+/* #region ------------------ C ------------------ */
+
+C::C()
+{
+}
+
+C::~C()
+{
+}
+
+/* #endregion */
+
+/* #region ------------------ Other Funcs ------------------ */
 
 Base	*generate( void )
 {
+	int	select;
+
 	srand(time(NULL));
-	switch ()
+	select = rand();
+	switch (select)
 	{
-		
+		case 0:
+			return (new A);
+		case 1:
+			return (new B);
+		case 2:
+			return (new C);
 	}
+	return (NULL);
 }
+
+/* #endregion */
--- module06/ex02/main.cpp
+++ module06/ex02/main.cpp
@@ -1,45 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   Base.hpp                                           :+:    :+:            */
+/*   main.cpp                                           :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: jhille <jhille@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2022/10/14 17:34:03 by jhille        #+#    #+#                 */
-/*   Updated: 2022/10/17 16:32:29 by jhille        ########   odam.nl         */
+/*   Created: 2022/10/17 15:28:13 by jhille        #+#    #+#                 */
+/*   Updated: 2022/10/17 15:31:39 by jhille        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BASE_HPP
-# define BASE_HPP
+#include "Base.hpp"
 
-class Base
+int	main()
 {
-	public:
-		virtual ~Base();
-};
+	Base	*ptr;
 
-class A : public Base
-{
-	public:
-		A();
-		~A();
-};
-
-class B : public Base
-{
-	public:
-		B();
-		~B();
-};
-
-class C : public Base
-{
-	public:
-		C();
-		~C();
-};
-
-Base	*generate( void );
-
-#endif
+	ptr = generate();
+}
