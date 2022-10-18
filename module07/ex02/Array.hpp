644b3396cfc89a896ee49153f18b486e1d96d06d
Justin Hille <jhille@f1r4s7.codam.nl>
2022-10-18 17:41:55 +0200

    started work on ex02

---
+++ module07/ex02/Array.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   Array.hpp                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jhille <jhille@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/10/18 17:06:19 by jhille        #+#    #+#                 */
+/*   Updated: 2022/10/18 17:18:06 by jhille        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+template < typename T >
+class Array
+{
+	
+	public:
+		Array();
+		~Array();
+	
+};
+
+
+
+#endif
